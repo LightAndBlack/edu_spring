6f51e54670188c91d58c119deced70e691e9b6ab
One <pavel.svitsenka@gmail.com>
2025-04-22 10:40:35 +0300

    инициация

---
+++ src/main/java/com/example/edu_spring/EduSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.edu_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSpringApplication.class, args);
+	}
+
+}
